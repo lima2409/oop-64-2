955b81ae3b753f22a5f20dbc46b0dd7ff9384f6d
Элима <elima@MacBook-Air-Elima.local>
2026-03-19 22:14:11 +0600

    Обновила hw5.py

--- hw/hw5.py
+++ hw/hw5.py
@@ -1,10 +1,30 @@
+def is_admin(func):
+    def wrapper(user: User):
+        if user.role == "admin":
+            return func(user)
+        else:
+            print("У вас нет доступа")
+    return wrapper
+
+
+@is_admin
+def delete_video(user: User):
+    print("Видео удалено")
+
+
+admin = User("Ardager", "admin")
+user = User("Bek", "user")
+
+delete_video(admin)
+delete_video(user)
+
 import time
 
 def timer(func):
     def wrapper():
-        start = time.time()      # начало
-        func()                   # выполнение функции
-        end = time.time()        # конец
+        start = time.time()
+        func()
+        end = time.time()
         print(f"Время выполнения: {round(end - start, 2)} секунд")
     return wrapper
 
